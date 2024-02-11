57cb95fe52b137190bf7e84578dd5581e862f932
gboutte <btte.gregory@gmail.com>
2024-02-11 21:57:44 +0100

    new method of component create

--- projects/glassui/src/lib/modal/components/modal/modal.component.ts
+++ projects/glassui/src/lib/modal/components/modal/modal.component.ts
@@ -1,8 +1,8 @@
 import {
   ApplicationRef,
-  Component,
+  Component, ComponentRef, createComponent, ElementRef, EmbeddedViewRef,
   EventEmitter, Injector,
-  Input,
+  Input, OnDestroy,
   OnInit,
   Output,
   Type,
@@ -16,23 +16,40 @@ import {ModalConfig} from "../../modal-config";
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit,OnDestroy {
   @Input() componentContent!: Type<any>;
-  @ViewChild('content', {read: ViewContainerRef, static: true}) //normal way
-  content!: ViewContainerRef;
+  @ViewChild('content', {read: ElementRef, static: true}) //normal way
+  content!: ElementRef;
   @Output() close = new EventEmitter();
   closed = false;
+
+  private componentRef!: ComponentRef<any>;
   constructor(
     protected modalConfig: ModalConfig,
     private injector: Injector,
+    private applicationRef: ApplicationRef,
   ) {
   }
 
   ngOnInit(): void {
     if (this.componentContent !== undefined) {
-      this.content.createComponent(this.componentContent,{
-        injector: this.injector
+
+      //Create the component
+      const component = createComponent(this.componentContent, {
+        environmentInjector: this.applicationRef.injector,
+        elementInjector: this.injector,
       });
+      this.applicationRef.attachView(component.hostView);
+
+
+      // Append the component to the modal to make it visible
+      const domElem = (component.hostView as EmbeddedViewRef<any>).rootNodes[0] as HTMLElement;
+      this.content.nativeElement.append(domElem);
+    }
+  }
+  ngOnDestroy() {
+    if(this.componentRef){
+      this.applicationRef.detachView(this.componentRef.hostView);
     }
   }
 
